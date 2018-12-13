c47f8fbc90f4732da4f3ee286612d21ba4f1bc11
VF <vfunes@hotmail.com>
2018-12-13 10:43:27 -0300

    otra vez lo de Juli

--- resources/views/presentarPregunta.blade.php
+++ resources/views/presentarPregunta.blade.php
@@ -15,47 +15,6 @@
         @endfor
       </ul>
 
-<<<<<<< HEAD
-{{--Cambio JT 13/12: TIMER--}}
-    <div class="timer">
-      {{10}}
-    </div>
-
-{{--Cambio JT 13/12: SOLO href="/calcularPuntaje":--}}
-    {{--}}    <a class="btnjugar" href="/calcularPuntaje">Salir</a>--}}
-      <input type="hidden" name="idPregunta" value="{{$idPregunta}}">
-  </form>
-
-  <form action="/presentarResultado" method="post" class="este">
-    @csrf
-      <input class="btncrear btn-opciones" type="submit" name="opcionElegida" value="salir">
-      <input type="hidden" name="idPregunta" value="{{$idPregunta}}">
-  </form>
-
-{{--Cambio JT 13/12: BLOQUE JS:--}}
-
-  <script type="text/javascript">
-  // window.addEventListener('DOMContentLoaded')
-    window.onload=function(){
-      var tiempoRestante=10;
-      var timer = document.querySelector('.timer');
-      var contador=setInterval(function(){
-        console.log('Bajamos de segundos');
-        timer.innerText = tiempoRestante;
-        tiempoRestante--;
-
-        if (timer.innerText==0){
-          clearInterval(contador);
-          {{--location.href ="/calcularPuntaje";--}}
-          document.querySelector('.este').submit();
-        }
-      }, 1000);
-    }
-  </script>
-
-
-  @endsection
-=======
       <a class="btnjugar btndejardejugar" href="/">Dejar de Jugar</a>
       {{--}}<input type="submit" name="opcionElegida" value="Salir">--}}
       <input type="hidden" name="idPregunta" value="{{$idPregunta}}">
@@ -65,4 +24,3 @@
 @section("fondo")
    {{$fondo}}
 @endsection
->>>>>>> daeeccc8d4051ae379175a93bed28fdee71338fe
